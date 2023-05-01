b7c66cea78091ba61ae9e425e46d88006ad86575
evilyn <evilyndeveloper@gmail.com>
2023-05-01 13:24:39 -0300

    responsividade

--- scripts/course.js
+++ scripts/course.js
@@ -6,49 +6,13 @@ const courses = [
     rating: 4.5,
     totalRatings: 100,
     points: 20000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/07/BANNER-EMAIL-MKT_CATALOGO-DE-FUNCIONALIDADES_1080-X-1080-1-768x768.png'
+    imageUrl: 'https://www.gov.br/inep/pt-br/assuntos/noticias/imagens/I_professora_no_pc.png/@@images/image'
   },
   {
     title: 'Panorama T&D',
     duration: '20h 30min',
     category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
-    totalRatings: 50,
-    points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/08/DIA_26_ARTIGO_KONVIVA_AGOSTO-720x761.png'
-  },
-  {
-    title: 'Panorama T&D',
-    duration: '20h 30min',
-    category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
-    totalRatings: 50,
-    points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/06/DIA_13_BLOG_JUNHO_KONVIVA_CAPA-720x761.png'
-  },
-  {
-    title: 'Análise Comportamental',
-    duration: '80h 30min',
-    category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
-    totalRatings: 50,
-    points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/04/DIA_08_BLOG_AZUL_CAPA-720x761.jpg'
-  },
-  {
-    title: 'Panorama T&D',
-    duration: '20h 30min',
-    category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
-    totalRatings: 50,
-    points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/08/DIA_26_ARTIGO_KONVIVA_AGOSTO-720x761.png'
-  },
-  {
-    title: 'Panorama T&D',
-    duration: '20h 30min',
-    category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
+    rating: 3.2,
     totalRatings: 50,
     points: 1000,
     imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2019/04/news-konviva.png'
@@ -57,46 +21,82 @@ const courses = [
     title: 'Panorama T&D',
     duration: '20h 30min',
     category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
+    rating: 3.5,
     totalRatings: 50,
     points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/08/DIA_26_ARTIGO_KONVIVA_AGOSTO-720x761.png'
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2019/04/news-ilog.png'
   },
   {
-    title: 'Treine sua liderança',
-    duration: '15h',
+    title: 'Análise Comportamental',
+    duration: '80h 30min',
     category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
+    rating: 3.1,
     totalRatings: 50,
     points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/06/22.03-CAPA-1-720x761.png'
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2018/08/04.jpg'
   },
   {
     title: 'Panorama T&D',
     duration: '20h 30min',
     category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
+    rating: 2.5,
+    totalRatings: 50,
+    points: 1000,
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2020/03/Informal-Learning-Blog.png'
+  },
+  {
+    title: 'Panorama T&D',
+    duration: '20h 30min',
+    category: 'Desenvolvimento pessoal e profissional',
+    rating: 3.8,
+    totalRatings: 50,
+    points: 1000,
+    imageUrl: 'https://elearningindustry.com/wp-content/uploads/2017/07/e66145fc679f0268ead15bbebea8be45-800x525-1-e1500903141602.png'
+  },
+  {
+    title: 'Panorama T&D',
+    duration: '20h 30min',
+    category: 'Desenvolvimento pessoal e profissional',
+    rating: 3.9,
+    totalRatings: 50,
+    points: 1000,
+    imageUrl: 'https://images.squarespace-cdn.com/content/v1/55eb4adbe4b097dd68b32ba4/1571557450473-5R3ZBMMOTXJCJ0VIZRT1/image2.jpg?format=1000w'
+  },
+  {
+    title: 'Treine sua liderança',
+    duration: '15h',
+    category: 'Desenvolvimento pessoal e profissional',
+    rating: 3.0,
+    totalRatings: 50,
+    points: 1000,
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/12/iStock-1320918242-720x480.jpg'
+  },
+  {
+    title: 'Panorama T&D',
+    duration: '20h 30min',
+    category: 'Desenvolvimento pessoal e profissional',
+    rating: 3.5,
     totalRatings: 150,
     points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/08/DIA_26_ARTIGO_KONVIVA_AGOSTO-720x761.png'
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2023/02/iStock-1366793164-720x326.jpg'
   },
   {
     title: 'Como utilizar a plataforma PID',
     duration: '20h 30min',
     category: 'Desenvolvimento pessoal e profissional',
-    rating: 800,
+    rating: 5.0,
     totalRatings: 50,
     points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/05/DIA_18_MAIO_BLOG_CAPA-720x761.png'
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2023/03/iStock-1358748397-720x405.jpg'
   },
   {
     title: 'Treine Times de 500 pessoas',
     duration: '20h 30min',
     category: 'Desenvolvimento pessoal e profissional',
-    rating: 3,
+    rating: 5.0,
     totalRatings: 50,
     points: 1000,
-    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2022/03/DIA_07_BLOG_CAPA-720x761.png'
+    imageUrl: 'https://www.konviva.com.br/wp-content/uploads/2023/03/iStock-1386241232-720x480.jpg'
   },
 ];
 
@@ -106,7 +106,7 @@ function createCourseElement(course) {
   courseElement.classList.add('card', 'd-flex', 'flex-column', 'rounded');
   courseElement.innerHTML = `
     <div class="top-card rounded-top">
-      <div class="booble-course">Curso</div>
+      <div class="d-flex booble-course">Curso</div>
       <img class="img-fluid rounded-top img-card" src="${course.imageUrl || "https://play-lh.googleusercontent.com/jgqE3RuyTNA0HXNtqZZUzW_jlM4u6x82D1IhjwZ7d0oBdiRdO2mzSZVetYbNYcl75yQ"}" alt="capa do curso">
     </div>
     <div class="bottom-card p-3 d-flex flex-column align-items-star justify-content-center">
@@ -149,5 +149,3 @@ function renderCourses(courses) {
 }
 
 renderCourses(courses)
-
-console.log(courses)
